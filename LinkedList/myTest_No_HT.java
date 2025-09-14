ac42f8e392e852173a63452e3cf0ac066df28121
Justin <justin.turner245@gmail.com>
2025-09-14 12:07:27 -0400

    Update myTest_No_HT.java

--- LinkedList/myTest_No_HT.java
+++ LinkedList/myTest_No_HT.java
@@ -1,8 +1,3 @@
-// Name:            Justin Turner
-// Class:	        CS 3305/W01
-// Term:		    Fall 2025
-// Instructor:      S. Perry
-// Assignment:      3
 // IDE Name:	    Intellij
 
 
@@ -261,3 +256,4 @@ public class myTest_No_HT
    }
 }
     
+
